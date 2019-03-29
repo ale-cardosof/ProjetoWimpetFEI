be277b2135446b082847c091cf1e441b8b0c5c8f
lucasserain <lucas.serain@outlook.com>
2019-03-29 01:26:51 -0300

    Problema com conexão, solucionando. Corrigido erros ao remover Activitys de View.

--- app/src/main/java/com/example/alexandrecardoso/projetowimpetfei/menuLogin.java
+++ app/src/main/java/com/example/alexandrecardoso/projetowimpetfei/menuLogin.java
@@ -1,18 +1,39 @@
 package com.example.alexandrecardoso.projetowimpetfei;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.alexandrecardoso.projetowimpetfei.Controller.Conexao;
 import com.example.alexandrecardoso.projetowimpetfei.R;
 
 public class menuLogin extends AppCompatActivity {
-
+    EditText inputUsuario, inputSenha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_login);
+        inputUsuario = findViewById(R.id.tvUsuario);
+        inputSenha = findViewById(R.id.tvSenha);
     }
+    public void login(View view){
+        if(Conexao.Conectar()){
+            Toast toast = Toast.makeText(this, "a", Toast.LENGTH_LONG);
+            toast.show();
+        }
 
+        String buscaNome = inputUsuario.getText().toString();
+        String buscaSenha = inputSenha.getText().toString();
+        if(Conexao.login(buscaNome, buscaSenha)){
+            Toast toast = Toast.makeText(this, "a", Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+    }
 
 }
