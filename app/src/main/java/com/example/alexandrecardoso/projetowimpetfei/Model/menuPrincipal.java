cf98f13cf1ce52b7068aa34cf26c1551d11b69b3
lucasserain <lucas.serain@outlook.com>
2019-03-28 22:31:09 -0300

    Tela menu principal, recursos projeto

--- app/src/main/java/com/example/alexandrecardoso/projetowimpetfei/Model/menuPrincipal.java
+++ app/src/main/java/com/example/alexandrecardoso/projetowimpetfei/Model/menuPrincipal.java
@@ -1,7 +1,9 @@
-package com.example.alexandrecardoso.projetowimpetfei;
+package com.example.alexandrecardoso.projetowimpetfei.Model;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.alexandrecardoso.projetowimpetfei.R;
 
 public class menuPrincipal extends AppCompatActivity {
 
