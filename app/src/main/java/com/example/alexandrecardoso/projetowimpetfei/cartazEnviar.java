49c24a9fd8c91c3a426a3a41891a4e596df62836
filipiguimaraes <fgsilva.ti@gmail.com>
2019-04-06 14:58:09 -0300

    Criação da tela de envio da imagem.

---
+++ app/src/main/java/com/example/alexandrecardoso/projetowimpetfei/cartazEnviar.java
@@ -0,0 +1,132 @@
+package com.example.alexandrecardoso.projetowimpetfei;
+
+import android.app.ProgressDialog;
+import android.app.VoiceInteractor;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnPausedListener;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageException;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.example.alexandrecardoso.projetowimpetfei.Model.Usuario;
+
+import java.io.IOException;
+
+public class cartazEnviar extends AppCompatActivity{
+    private static final int PICK_IMAGE_REQUEST = 234;
+    private ImageView imvImagem;
+    private Button btnEscImagem, btnUpload;
+
+    private Uri caminho;
+    private StorageReference storageReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cartaz);
+
+        btnEscImagem = findViewById(R.id.btnEscImagem);
+        btnUpload = findViewById(R.id.btnUpload);
+        imvImagem = findViewById(R.id.imvImagem);
+        storageReference = FirebaseStorage.getInstance().getReference();
+        btnEscImagem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                escolherImagem();
+            }
+        });
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Upload do arquivo
+                uploadImagem();
+            }
+        });
+    }
+
+    private void escolherImagem(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Escolha uma foto."), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null){
+
+            caminho = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), caminho);
+                imvImagem.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void uploadImagem(){
+        if (caminho != null){
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Enviando...");
+            StorageReference riversRef = storageReference.child("images/profile.jpg");
+
+            riversRef.putFile(caminho)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(),"Arquivo enviado com sucesso!", Toast.LENGTH_LONG);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(),exception.getMessage(), Toast.LENGTH_LONG);
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>(){
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot){
+                            double progress = (100 * taskSnapshot.getBytesTransferred())/taskSnapshot.getTotalByteCount();
+                            progressDialog.setMessage(((int) progress)+"% enviados...";
+                        }
+                    });
+        }
+    }
+}
